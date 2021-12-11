b564dc86bf7168259f8994f2ee8a475f9d83c921
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-12-11 17:30:02 +0100

    Add rollbar configuration

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,6 +1,17 @@
 import { cleanEnv, port, str } from 'envalid';
+import Rollbar from 'rollbar';
+import config from 'config';
 
 const validateEnv = () => {
+  const rollbar = new Rollbar({
+    accessToken: config.get('rollbarToken'),
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  })
+  // record a generic message and send it to Rollbar
+  rollbar.log('Hello world!')
+  console.log("message sent")
+
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
